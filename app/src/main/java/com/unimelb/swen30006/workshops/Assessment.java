67a899adeee3609f547963560dbb21876567ae00
zhye4 <zhou.ye1@student.unimelb.edu.au>
2025-03-25 17:37:56 +1100

    create calss assessment

---
+++ app/src/main/java/com/unimelb/swen30006/workshops/Assessment.java
@@ -0,0 +1,51 @@
+package com.unimelb.swen30006.workshops;
+
+import java.util.Date;
+
+public class Assessment {
+    private String description;
+    private Date dueDate;
+    private int maxAttempts;
+    private String name;
+
+    public Assessment(String description, Date dueDate, int maxAttempts, String name) {
+        this.description = description;
+        this.dueDate = dueDate;
+        this.maxAttempts = maxAttempts;
+        this.name = name;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getMaxAttempts() {
+        return maxAttempts;
+    }
+
+    public void setMaxAttempts(int maxAttempts) {
+        this.maxAttempts = maxAttempts;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
